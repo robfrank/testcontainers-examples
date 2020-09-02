eb58e4a0514ac71da04e123fe7f484ba64f49274
robfrank <roberto.franchini@alfresco.com>
2020-09-02 22:53:17 +0200

    updates orientdb to 3.1.2
    adds --quiet to mvn command

--- src/main/docker/orientdb/build.sh
+++ src/main/docker/orientdb/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O ./openbeer.zip  https://orientdb.com/public-databases/OpenBeer.zip  \
+wget -O ./openbeer.zip  https://orientdb.com/public-databases/3.0.x/OpenBeer.zip  \
  && mkdir -p ./openbeer \
  && unzip -d ./openbeer ./openbeer.zip \
  && rm ./openbeer.zip
